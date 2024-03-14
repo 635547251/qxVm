d226fcaa4419f9d010e3712178080c3bfa3a9e9f
Phil666 <45195707+635547251@users.noreply.github.com>
2024-03-14 15:15:08 +0800

    第二题

---
+++ match2023/match2/match2Vm.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const QXVM_GENERATE = require('../../qxVm_sanbox/qxVm.sanbox');
+
+
+function ReadCode(name, dir) {
+    let file_path = dir === undefined ? `${__dirname}/${name}` : `${__dirname}/${dir}/${name}`;
+    console.log(file_path)
+    return fs.readFileSync(file_path) + "\r\n"
+}
+
+
+const js_code = ReadCode(`/match2.js`);
+const user_config = {
+    isTest: true,
+    compress: false,
+    runConfig: { proxy: true, logOpen: true },
+    window_attribute: {
+        login: true
+    },
+
+    env: {
+        location: {
+            href: "https://match2023.yuanrenxue.cn/topic/2"
+        },
+    }
+}
+
+// 打印帮助信息
+QXVM_GENERATE.help()
+
+let result = QXVM_GENERATE.sanbox(js_code, "get_dddd", user_config);
+console.log(result.get_dddd())
